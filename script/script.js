59c9555c3b95d47abb35a5f109a238baff4c3021
santigiamba <sgiambartolomeichavarria@escuelasproa.edu.ar>
2024-03-02 16:35:33 -0300

    listo el trabajo en equipo

--- script/script.js
+++ script/script.js
@@ -1,26 +1,108 @@
-let combination = [0,0]
+let combination = ['n','n']
+let botonesI = document.querySelectorAll(".mine");
+let botonesD = document.querySelectorAll(".real");
+let mezclarBotton = document.getElementById("mezclar");
+let reiniciar = document.getElementById("reiniciar")
+let puntajeText = document.getElementById('puntaje')
+let puntaje = 0
+let minecraft = [
+    {
+        url: 'https://tse1.mm.bing.net/th/id/OIP.Bbxg0MzSY3IC8cKKisieGAAAAA?rs=1&pid=ImgDetMain',
+        id: '0i',
+        value: '0'
+    },
+    {
+        url: 'https://tse1.mm.bing.net/th/id/OIP.KYkR19B0B0X6Md5FtlsfewAAAA?rs=1&pid=ImgDetMain',
+        id: '1i',
+        value: '1'
+    },
+    {
+        url: 'https://tse3.mm.bing.net/th/id/OIP.191lHaRzPklSLvjjVHPSEAAAAA?rs=1&pid=ImgDetMain',
+        id: '2i',
+        value: '2'
+    },
+    {
+        url:'https://tse4.mm.bing.net/th/id/OIP.fie5NHyvlU5EnfJQ6X0IfwAAAA?rs=1&pid=ImgDetMain',
+        id: '3i',
+        value: '3'
+    }
+]
 
-let minecraft = []
 
+    let real = [
+    {
+        url: 'https://www.pawnshop.es/wp-content/uploads/2018/12/diamond-500872_1280.jpg',
+        id: '0d',
+        value: '0'
+    },
+    {
+        url: 'https://tse4.mm.bing.net/th/id/OIP.j7d7ScSqV6PyboRJSxqL5QHaGH?rs=1&pid=ImgDetMain',
+        id: '1d',
+        value: '1'
+    },
+    {
+        url:'https://th.bing.com/th/id/R.f9d3c06d4fa79b01f7a61b45622d694d?rik=hVbDJb1J9JoSFQ&pid=ImgRaw&r=0',
+        id: '2d',
+        value: '2'
+    },
+    {
+        url:'https://mineralesyrocas.com/wp-content/uploads/rubi.jpg',
+        id: '3d',
+        value: '3'
+    }]
+   
 
-var botonesI = document.querySelectorAll(".mine");
-var botonesD = document.querySelectorAll(".real");
+  function mix(arreglo) {
+    arreglo.sort(function() {
+      return Math.random() - 0.5;
+    });
+  }
+
 
 function checkCombination(){
-    if(combination[0] != 0 && combination[1] != 0){
+    if(combination[0] != 'n' && combination[1] != 'n'){
         if(combination[0] === combination[1]){
             alert("Bien hecho!  😁😁😁😁🤩💯💯")
-            combination = [0,0]
+            combination = ['n','n']
+            puntaje += 100
+            puntajeText.innerText = puntaje
             return true
         }
         else{
             alert("Respuesta incorrecta ❌❌🚫 Sigue Intentando ")
-            combination = [0,0]
+            combination = ['n','n']
+            puntaje -= 100
+            puntajeText.innerText = puntaje
             return false
         }
 
     }
 }
+
+
+mezclarBotton.addEventListener("click", function(){
+    puntajeText.innerText = puntaje
+mix(minecraft)
+mix(real)
+
+botonesI.forEach(function(botonI, i) {
+    let materialI = minecraft[i];
+
+    botonI.querySelector("img").src = materialI.url;
+    botonI.id = materialI.id;
+    botonI.value = materialI.value;
+});
+
+
+botonesD.forEach(function(botonD, i) {
+    let materialD = real[i];
+
+    botonD.querySelector("img").src = materialD.url;
+    botonD.id = materialD.id;
+    botonD.value = materialD.value;
+});
+
+})
 botonesI.forEach(function(botonI) {
   botonI.addEventListener("click", function() {
     combination[0] = botonI.value;
@@ -34,6 +116,7 @@ botonesI.forEach(function(botonI) {
   });
 });
 
+
 botonesD.forEach(function(botonD) {
     botonD.addEventListener("click", function() {
         combination[1] = botonD.value;
@@ -44,3 +127,8 @@ botonesD.forEach(function(botonD) {
         }
     });
   });
+
+
+reiniciar.addEventListener("click", function() {
+    location.reload();
+});
